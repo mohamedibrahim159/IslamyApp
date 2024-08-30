b80783d3af7efaf51227efed9535ab52d40ef557
Homossssssa <mohamed7ibrahim159@gmail.com>
2024-08-31 00:58:39 +0300

    change colors

--- lib/Sebha_tap/sebha_tap.dart
+++ lib/Sebha_tap/sebha_tap.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../Providers/app_config_provider.dart';
 
 class sebhaTap extends StatefulWidget {
   sebhaTap({super.key});
@@ -9,97 +12,100 @@ class sebhaTap extends StatefulWidget {
 
 class _sebhaTapState extends State<sebhaTap> {
   int counter = 0;
-  String text = "";
+  double angle = 0;
+  int index = 0;
+
+  List<String> Tasbehlist = [
+    "سبحان الله",
+    "الحمد لله",
+    "الله اكبر",
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    var provider = Provider.of<appConfigProvider>(context);
+
+    return Container(
+        child: Column(
       children: [
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Column(
+        Center(
+          child: Stack(
             children: [
-              Container(
-                child: Image.asset("assets/images/head_sebha_logo.png"),
+              Padding(
+                padding: EdgeInsets.only(top: 80),
+                child: Transform.rotate(
+                    angle: angle,
+                    child: provider.isDark()
+                        ? Image.asset("assets/images/body_sebha_dark.png")
+                        : Image.asset("assets/images/body_sebha_logo.png")),
               ),
-              Container(
-                child: Image.asset("assets/images/body_sebha_logo.png"),
-              ),
+              Padding(
+                padding: EdgeInsetsDirectional.only(start: 80),
+                // استخدام start بدلاً من left
+                child: provider.isDark()
+                    ? Image.asset("assets/images/head_sebha_dark.png")
+                    : Image.asset("assets/images/head_sebha_logo.png"),
+              )
             ],
-          )
-        ]),
+          ),
+        ),
         SizedBox(
-          height: 50,
+          height: 45,
         ),
         Text(
           "عدد التسبيحات",
-          style: Theme.of(context).textTheme.titleLarge,
+          style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w600,
+              fontFamily: 'El Messiri'),
         ),
         SizedBox(
           height: 30,
         ),
         Container(
-          height: 70,
-          width: 55,
+          padding: EdgeInsets.all(25),
           decoration: BoxDecoration(
-              color: Color(0xffB7935F),
-              borderRadius: BorderRadius.horizontal(
-                  right: Radius.elliptical(25, 25),
-                  left: Radius.elliptical(25, 25))),
-          child: Center(
-              child: Text(
+              color: provider.isDark() ? Color(0xffFACC1D) : Color(0xffFB7935F),
+              borderRadius: BorderRadius.circular(25)),
+          child: Text(
             "$counter",
-            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 25),
-          )),
+            style: TextStyle(fontSize: 25),
+          ),
         ),
         SizedBox(
-          height: 20,
+          height: 35,
         ),
-        InkWell(
-          onTap: () {
-            counterText();
-            counter = counter + 1;
-            initCount();
-            setState(() {});
-          },
-          child: Container(
-            height: 60,
-            width: 250,
-            decoration: BoxDecoration(
-                color: Color(0xffB7935F),
-                borderRadius: BorderRadius.horizontal(
-                    right: Radius.elliptical(45, 45),
-                    left: Radius.elliptical(45, 45))),
-            child: Center(
-                child: Text(
-              // counter==0?
-              // "سبحان الله"
-              //     :
-              text,
-              style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                    fontWeight: FontWeight.w500,
-                  ),
-            )),
-          ),
+        Container(
+          padding: EdgeInsets.all(25),
+          decoration: BoxDecoration(
+              color: provider.isDark() ? Color(0xffFACC1D) : Color(0xffFB7935F),
+              borderRadius: BorderRadius.circular(45)),
+          child: InkWell(
+              onTap: () {
+                counter++;
+                tasbeh();
+                angle += 0.7;
+              },
+              child: Text(
+                "${Tasbehlist[index]}",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                    fontFamily: 'El Messiri'),
+              )),
         )
       ],
-    );
+    ));
   }
 
-  void counterText() {
-    if (counter == 0 || counter <= 33) {
-      text = "سبحان الله";
-    } else if (counter == 34 || counter <= 66) {
-      text = "الحمد لله";
-    } else if (counter == 67 || counter <= 99) {
-      text = "لا إله إلا الله";
-    } else if (counter == 99) {
-      text = "الله أكبر";
-    }
-  }
-
-  void initCount() {
-    if (counter == 100) {
+  void tasbeh() {
+    if (counter == 33) {
+      index++;
       counter = 0;
     }
+    if (index > 2) {
+      index = 0;
+    }
+    setState(() {});
   }
 }
